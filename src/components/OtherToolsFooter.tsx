c4d3d3c7d0d74ffbc6b59938280a1190847a1f6d
JonasDoesThings <jonas@jonasdoesthings.com>
2025-07-04 15:27:22 +0200

    refactor: add aria-label to shared footer

--- src/components/OtherToolsFooter.tsx
+++ src/components/OtherToolsFooter.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export function OtherToolsListFooter() {
   return (
-    <section className='px-4 pb-16'>
+    <section aria-label='more-tools' className='px-4 pb-16'>
       <h2 className='text-lg font-bold'>Other Free PFP Editing Tools:</h2>
       <ul className='list-disc list-inside [&_a]:underline [&_a]:text-accent'>
         <li><Link href='/'>AI-Powered PFP Creator</Link></li>
